e83f9efb7a30d2b6dce2afd8fcaf9bf1fe2e411a
Francisco Fernandez <arkhadi@gmail.com>
2019-05-19 17:00:42 +0100

    Second Cycle

--- src/test/java/com/codurance/YearTest.java
+++ src/test/java/com/codurance/YearTest.java
@@ -1,13 +1,22 @@
 package com.codurance;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class YearTest {
 
-    @Test
-    void shouldBeALeapYearIfItIsDivisibleByFour() {
-        assertThat(Year.isLeapYear(1996)).isEqualTo(true);
+    @ParameterizedTest
+    @ValueSource(ints = {1996, 2000 ,2008})
+    void shouldBeALeapYearIfItIsDivisibleByFour(int input) {
+        assertThat(Year.isLeapYear(input)).isEqualTo(true);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1997, 2002 ,2011})
+    void shouldNotBeLeapYearIfNotDivisibleByFour(int input) {
+        assertThat(Year.isLeapYear(input)).isEqualTo(false);
     }
 }
