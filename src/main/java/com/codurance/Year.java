0d675fbef3636928468a6d8f738e0f7a860b8586
Francisco Fernandez <arkhadi@gmail.com>
2019-06-08 17:56:17 +0100

    Third Cycle

--- src/main/java/com/codurance/Year.java
+++ src/main/java/com/codurance/Year.java
@@ -3,6 +3,9 @@ package com.codurance;
 public class Year {
 
     static boolean isLeapYear(int year){
+        if(year % 400 == 0) {
+            return true;
+        }
         return year % 4 == 0;
     }
 }
--- src/test/java/com/codurance/YearTest.java
+++ src/test/java/com/codurance/YearTest.java
@@ -9,14 +9,20 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 public class YearTest {
 
     @ParameterizedTest
-    @ValueSource(ints = {1996, 2000 ,2008})
+    @ValueSource(ints = {1996, 2000, 2008})
     void shouldBeALeapYearIfItIsDivisibleByFour(int input) {
-        assertThat(Year.isLeapYear(input)).isEqualTo(true);
+        assertThat(Year.isLeapYear(input)).isTrue();
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {1997, 2002 ,2011})
+    @ValueSource(ints = {1997, 2002, 2011})
     void shouldNotBeLeapYearIfNotDivisibleByFour(int input) {
-        assertThat(Year.isLeapYear(input)).isEqualTo(false);
+        assertThat(Year.isLeapYear(input)).isFalse();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1600, 2000, 2400})
+    void shouldBeLeapYearIfItIsDivisibleBy400(int input) {
+        assertThat(Year.isLeapYear(input)).isTrue();
     }
 }
